57ba7436f8a450d19519b481f660e41370300198
Jacques Lucke <mail@jlucke.com>
2020-12-29 15:41:54 +0100

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,29 @@
+#include <Arduino.h>
+#include <NeoPixelBus.h>
+
+constexpr int pixel_count = 1;
+
+/* Uses pin GPIO3 on esp8266 due to hardware issues. This is pin RXit . */
+NeoPixelBus<NeoRgbFeature, Neo800KbpsMethod> pixels{pixel_count};
+
+void setup() {
+  Serial.begin(9600);
+
+  pixels.Begin();
+  pixels.Show();
+  // put your setup code here, to run once:
+}
+
+static void updateLEDs(uint8_t v1, uint8_t v2, uint8_t v3) {
+  pixels.SetPixelColor(0, RgbColor(v1, v2, v3));
+  pixels.Show();
+}
+
+void loop() {
+  updateLEDs(255, 20, 20);
+  delay(1000);
+  updateLEDs(20, 255, 20);
+  delay(1000);
+  updateLEDs(20, 20, 255);
+  delay(1000);
+}
