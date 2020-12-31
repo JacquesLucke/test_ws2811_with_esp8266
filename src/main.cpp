028ca0fe3cc707b816bb848b8d507a3f16eb0432
Jacques Lucke <mail@jlucke.com>
2020-12-31 14:41:11 +0100

    connect more leds

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
 #include <Arduino.h>
 #include <NeoPixelBus.h>
 
-constexpr int pixel_count = 1;
+constexpr int pixel_count = 3;
+constexpr int led_count = pixel_count * 3;
 
 /* Uses pin GPIO3 on esp8266 due to hardware issues. This is pin RXit . */
 NeoPixelBus<NeoRgbFeature, Neo800KbpsMethod> pixels{pixel_count};
@@ -10,19 +11,44 @@ void setup() {
   Serial.begin(9600);
 
   pixels.Begin();
+  pixels.ClearTo(RgbColor(0));
   pixels.Show();
 }
 
-static void updateLEDs(uint8_t v1, uint8_t v2, uint8_t v3) {
-  pixels.SetPixelColor(0, RgbColor(v1, v2, v3));
+static void set_led_color(int led, uint8_t value) {
+  const int pixel_index = led / 3;
+  const int channel_index = 2 - (led % 3);
+  RgbColor color = pixels.GetPixelColor(pixel_index);
+  ((uint8_t *)&color)[channel_index] = value;
+  pixels.SetPixelColor(pixel_index, color);
+}
+
+static void led_effect__all_off(const int delay_in_ms) {
+  pixels.ClearTo(RgbColor(0));
   pixels.Show();
+  delay(delay_in_ms);
+}
+
+static void led_effect__enable_one_after_the_other(const int delay_in_ms) {
+  pixels.ClearTo(RgbColor(0));
+  for (int i = 0; i < led_count; i++) {
+    set_led_color(i, 255);
+    pixels.Show();
+    delay(delay_in_ms);
+  }
+}
+
+static void led_effect__shift_single(const int delay_in_ms) {
+  for (int i = 0; i < led_count; i++) {
+    pixels.ClearTo(RgbColor(0));
+    set_led_color(i, 255);
+    pixels.Show();
+    delay(delay_in_ms);
+  }
 }
 
 void loop() {
-  updateLEDs(255, 20, 20);
-  delay(1000);
-  updateLEDs(20, 255, 20);
-  delay(1000);
-  updateLEDs(20, 20, 255);
-  delay(1000);
+  led_effect__all_off(100);
+  led_effect__enable_one_after_the_other(100);
+  led_effect__shift_single(100);
 }
